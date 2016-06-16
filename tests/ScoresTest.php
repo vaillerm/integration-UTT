2a9f7415d98d081e68050ffd5f35f3358e914e9d
Aurélien Labate <aurelien-dev@labate.me>
2016-06-16 16:12:40 +0200

    Add travis-ci

---
+++ tests/ScoresTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ScoresTest extends TestCase
+{
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->visit('/scores')
+             ->see('Que la meilleure faction gagnge !');
+    }
+}
