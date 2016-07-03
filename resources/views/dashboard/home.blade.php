7a619cd14f189c850211dc67dad4d07fdfd70a2f
Aurélien Labate <aurelien-dev@labate.me>
2016-07-03 13:46:29 +0200

    Update dashboard homepage because all volunteers can now see it

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -5,16 +5,28 @@ Accueil
 @endsection
 
 @section('smalltitle')
-Mises à jour, informations, ...
+Informations diverses pour toi !
 @endsection
 
 @section('content')
 <div class="box box-default">
-    <div class="box-header with-border">
-        <h3 class="box-title">20/07/2015 — Site opérationnel</h3>
-    </div>
-    <div class="box-body">
-        Toutes les fonctionalités prévues sont opérationnelles !</b>
-    </div>
+
+        <div class="box-header with-border">
+            <h3 class="box-title">Bonjour à toi bénévole !</h3>
+        </div>
+        <div class="box-body">
+            <p>Si tu vois ce message, c'est que tu fais partie de la grande famille des bénévoles de l'intégration. <strong>Et nous t'en remercions !</strong></p>
+
+            <p>Tu te trouves sur le panneau de gestion du site de l'intégration.
+                Ta mission la plus importante sur ce site est de tenir à jour ton numéro de téléphone et ton email afin de pouvoir être contacté lorsque l'intégration a besoin de toi. Tu peux les modifier en allant dans <strong>Mon compte > Mon profil bénévole</strong>.
+            </p>
+
+            @if (EtuUTT::student()->isAdmin())
+                <p>
+                    Comme tu es administrateur, ce site te permettra de faire pleins d'autres truc, mais j'ai la flème de tous les décrire.
+                    Du coup je te laisse faire joujoux avec ;) <br/> <em>Enfin.. evite de tout casser, hein !</em>
+                </p>
+            @endif
+        </div>
 </div>
 @endsection
