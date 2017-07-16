666a9b3956da6172d43cdb17bc485f30a19e603b
Hugo Collignon <hugo.collignon@uttnetgroup.fr>
2017-07-16 13:09:49 +0200

    Update list.blade.php

--- resources/views/dashboard/students/list.blade.php
+++ resources/views/dashboard/students/list.blade.php
@@ -16,7 +16,7 @@ Liste de tous les étudiants inscrits sur le site en tant que parrain, CE, orga.
         Pour qu'une personne apparaisse dans cette liste, il suffit qu'elle se soit connectée au site de l'intégration via le site étudiant au moins une fois.
     </p>
     <p>
-        Les personnes avec le label <span class="label label-success">Parrain</span> sont les personnes qui ont vu au moins une fois le formulaire de parrainnage. Ce ne sont donc pas forcément des parrains.<br/>
+        Les personnes avec le label <span class="label label-success">Parrain</span> sont les personnes qui ont vu au moins une fois le formulaire de parrainage. Ce ne sont donc pas forcément des parrains.<br/>
         Les personnes avec le label <span class="label label-info">Bénévole</span> sont les personnes qui ont accepté d'être contacté pour aider pendant l'intégration et qui ont donc fourni un téléphone et une adresse mail.<br/>
         Pour être <span class="label label-primary">CE</span>, <span class="label label-danger">Admin</span> et/ou <span class="label label-warning">Orga</span>, l'étudiant est obligé de devenir bénévole afin d'être sûr que l'intégration dispose de ses informations de contact.<br/>
         Pour qu'un étudiant soit <span class="label label-danger">Admin</span> et/ou <span class="label label-warning">Orga</span> un administrateur doit modifier le profil de l'étudiant via cette page.<br/>
@@ -70,7 +70,7 @@ Liste de tous les étudiants inscrits sur le site en tant que parrain, CE, orga.
                             @if ($student->volunteer)
                                 <span class="label label-info">Bénévole</span>
                             @endif
-                            @if ($student->orga)
+                            @if ($student->orga)https://gitlab.uttnetgroup.fr/
                                 <span class="label label-warning">Orga</span>
                             @endif
                         </td>
