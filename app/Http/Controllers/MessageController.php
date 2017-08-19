aa560ae52788a1413cf3317a576eff094b38dbda
prudywsh <antoineprudhomme5@gmail.com>
2017-08-19 20:21:57 +0200

    seend request to ionic's push api from server

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Message;
 use App\Models\Student;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Request;
 use Response;
 use Validator;
@@ -44,6 +46,24 @@ class MessageController extends Controller
 
         // create the message
         $message = Auth::guard('api')->user()->messages()->create(Request::all());
+
+        $notificationTargets = Student::where('admin', '!=', 0)->pluck('device_token')->toArray();
+
+        $client = new Client();
+        $result = $client->post('https://api.ionic.io/push/notifications', [
+            "body" => json_encode([
+                "tokens" => $notificationTargets,
+                "profile" => env("IONIC_PUSH_PROFILE"),
+                "notification" => [
+                    "message" => "Nouveau message dans '".Request::get('channel')."'"
+                ]
+            ]),
+            "headers" => [
+                'content-type' => 'application/json',
+                "Authorization" => "Bearer ".env("IONIC_API_TOKEN")
+            ],
+            'debug' => true
+        ]);
 
         return Response::json($message);
     }
