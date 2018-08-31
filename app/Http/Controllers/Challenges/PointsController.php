e1db19b9121ce712a228ca1f2ed22292beec05d2
Baptiste Prunot <baptiste.prunot@utt.fr>
2018-08-31 11:48:32 +0200

    Added form to add points

--- app/Http/Controllers/Challenges/PointsController.php
+++ app/Http/Controllers/Challenges/PointsController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Challenges;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 
 class PointsController extends Controller
 {
     public function manage() {
-        return view("dashboard.challenges.points");
+        $teams = Team::all();
+        return view("dashboard.challenges.points", compact("teams"));
     }
 }
--- resources/views/dashboard/challenges/points.blade.php
+++ resources/views/dashboard/challenges/points.blade.php
@@ -1,5 +1,36 @@
 @extends("layouts.auto")
 
+@section("title")
+    Ajouter des points
+@endsection
+
+@section("smalltitle")
+    Une équipe est particulièrement efficace ? DONNE LES POINTS !!!
+@endsection
+
 @section("content")
-    salut
+    <div class="box box-default">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="rsn" >Motif</label>
+                <input id="rsn" name="reason" class="form-control" type="text">
+            </div>
+            <div class="form-group">
+                <label for="pts">Nombre de points</label>
+                <input id="pts" class="form-control" type="number" name="amount">
+            </div>
+            <div class="form-group">
+                <label for="team">Équipe</label>
+                <select class="form-control" id="team" name="team_id">
+                    @foreach($teams as $id => $team)
+                        <option value="{{ $team->id }}">{{ $team->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="name">Auteur</label>
+                <input id="name" class="form-control" name="don't try to edit dis shit, it just won't work lmfao" type="text" value="{{ Auth::user()->first_name." ".Auth::user()->last_name }}" disabled>
+            </div>
+        </form>
+    </div>
 @endsection
