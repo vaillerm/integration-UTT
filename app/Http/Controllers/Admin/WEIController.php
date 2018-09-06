d7a68bd312cc01b32ee05e5ed45c6feb823a7c93
Aurélien Labate <aurelien@labate.me>
2018-09-06 22:10:41 +0200

    Try to dirty fix

--- app/Http/Controllers/Admin/WEIController.php
+++ app/Http/Controllers/Admin/WEIController.php
@@ -61,8 +61,8 @@ class WEIController extends Controller
     public function adminBusList(Request $request)
     {
         $buses = User::where('wei', 1)->get()->groupBy('bus_id')->sort();
-        $buses->put(0,$buses->get(0)->merge($buses->get("")));
-        $buses->forget("");
+        // $buses->put(0,$buses->get(0)->merge($buses->get("")));
+        // $buses->forget("");
         return view('dashboard.wei.bus-list', compact('buses'));
     }
 
