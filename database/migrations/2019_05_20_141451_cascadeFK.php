758a563dd4d735f5bcbebfcdc418c17e2bc64c91
Christian d'Autume <christian@dautume.fr>
2019-05-20 14:46:10 +0200

    :bomb: Fuck of model name

--- database/migrations/2019_05_20_141451_cascadeFK.php
+++ database/migrations/2019_05_20_141451_cascadeFK.php
@@ -14,7 +14,7 @@ class CascadeFK extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropForeign(['team_id']);
+            $table->dropForeign('students_team_id_foreign');
             $table->foreign('team_id')
                 ->references('id')->on('teams')
                 ->onDelete('cascade');
