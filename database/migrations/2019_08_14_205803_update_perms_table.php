fdeb3d21c4689aee1d0c1e54a00d3ca869abee96
Christian d'Autume <christian@dautume.fr>
2019-08-20 10:43:46 +0200

    Correct migration fix

--- database/migrations/2019_08_14_205803_update_perms_table.php
+++ database/migrations/2019_08_14_205803_update_perms_table.php
@@ -1,40 +1,40 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class UpdatePermsTable extends Migration
 {
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::table('perms', function (Blueprint $table) {
-      $table->dropColumn('free_join');
-      $table->text('open')->nullable()->change();
-    });
-    Schema::table('perm_users', function (Blueprint $table) {
-      $table->text('presence')->nullable()->change();
-      $table->text('commentary')->nullable()->change();
-      $table->text('absence_reason')->nullable()->change();
-      $table->text('pointsPenalty')->nullable()->change();
-    });
-  }
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('perms', function (Blueprint $table) {
+            $table->dropColumn('free_join');
+            $table->text('open')->nullable();
+        });
+        Schema::table('perm_users', function (Blueprint $table) {
+            $table->text('presence')->nullable()->change();
+            $table->text('commentary')->nullable()->change();
+            $table->text('absence_reason')->nullable()->change();
+            $table->text('pointsPenalty')->nullable()->change();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::table('perms', function (Blueprint $table) {
-      $table->boolean('free_join')->default(false);
-      $table->dropColumn('open');
-    });
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('perms', function (Blueprint $table) {
+            $table->boolean('free_join')->default(false);
+            $table->dropColumn('open');
+        });
+    }
 }
