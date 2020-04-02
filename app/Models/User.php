4095b363723e1a109376c0b26e3c6f013c2ef5fb
Jonas DOREL <jonas@dorel.me>
2020-04-02 16:02:18 +0200

    Fix spelling

--- app/Models/User.php
+++ app/Models/User.php
@@ -255,7 +255,7 @@ class User extends Model implements Authenticatable
             'description' => 'Organiser les activités du rallye du Jeudi après-midi, regroupant toutes les équipes de nouveaux.',
         ],
         'wei' => [
-            'title' => 'Coordinateurs du WEI',
+            'title' => 'Coordinateur du WEI',
             'description' => 'Organiser le WEI.',
         ],
         'benevole' => [
