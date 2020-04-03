1057a382b8c5159d2a32575e0dc55baf222f5d8b
Jonas DOREL <jonas@dorel.me>
2020-04-03 17:44:33 +0200

    Fix library change

--- app/Console/Commands/ImportNewcomers.php
+++ app/Console/Commands/ImportNewcomers.php
@@ -39,7 +39,7 @@ class ImportNewcomers extends Command
      *
      * @return mixed
      */
-    public function fire()
+    public function handle()
     {
         dispatch(new refreshNewcomers());
     }
